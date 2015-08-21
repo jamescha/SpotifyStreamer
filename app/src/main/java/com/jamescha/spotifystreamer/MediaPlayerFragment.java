51f87c84f238a79d8af596e9936020d4b1233973
jamescha <james.cha.zero@gmail.com>
2015-08-21 10:54:43 -0700

    Adding Support for seekbar

--- app/src/main/java/com/jamescha/spotifystreamer/MediaPlayerFragment.java
+++ app/src/main/java/com/jamescha/spotifystreamer/MediaPlayerFragment.java
@@ -1,26 +1,62 @@
 package com.jamescha.spotifystreamer;
 
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.jamescha.spotifystreamer.mediaPlayer.MediaPlayerService;
 import com.squareup.picasso.Picasso;
 
+import java.util.concurrent.TimeUnit;
+
 public class MediaPlayerFragment extends DialogFragment {
     private static final String LOG_TAG = MediaPlayerFragment.class.getSimpleName();
-    private static final String ACTION_PLAY = "com.jamescha.spotifystreamer.action.PLAY";
-    private static final String ACTION_PAUSE = "com.jamescha.spotifystreamer.action.PAUSE";
     private Intent mediaPlayerIntent;
     private Boolean playPause = true;
+    private BroadcastReceiver receiver;
+    private SeekBar scrubBar;
+    TextView duration;
+    TextView fullTrackDuration;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int currentTime = intent.getIntExtra(MediaPlayerService.CURRENT_POSITION, 0);
+                int trackLength = intent.getIntExtra(MediaPlayerService.TRACK_LENGTH, 0);
+
+                if(scrubBar != null) {
+                    scrubBar.setMax(trackLength);
+                    scrubBar.setProgress(currentTime);
+                }
+
+//                if(duration != null) {
+//                    duration.setText(currentTime);
+//                }
+//
+                if(fullTrackDuration != null) {
+                    fullTrackDuration.setText(String.valueOf(TimeUnit.MILLISECONDS.toMinutes(trackLength)));
+                }
+
+            }
+        };
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
@@ -34,24 +70,32 @@ public class MediaPlayerFragment extends DialogFragment {
         TextView artistName = (TextView) view.findViewById(R.id.media_player_artist_name);
         TextView albumName = (TextView) view.findViewById(R.id.media_player_album_name);
         TextView trackName = (TextView) view.findViewById(R.id.media_player_track_name);
-        TextView duration = (TextView) view.findViewById(R.id.media_player_duration);
-        TextView fullTrackDuration = (TextView) view.findViewById(R.id.media_player_song_full_length);
+        duration = (TextView) view.findViewById(R.id.media_player_duration);
+        fullTrackDuration = (TextView) view.findViewById(R.id.media_player_song_full_length);
 
         ImageButton previousButton = (ImageButton) view.findViewById(R.id.media_player_previous);
         ImageButton nextButton = (ImageButton) view.findViewById(R.id.media_player_next);
         final ImageButton playPauseButton = (ImageButton) view.findViewById(R.id.media_player_play_pause);
+        scrubBar = (SeekBar) view.findViewById(R.id.media_player_scrub_bar);
+        scrubBar.setClickable(false);
 
-        ImageView scrubBar = (ImageView) view.findViewById(R.id.media_player_scrub_bar);
         ImageView songImageView = (ImageView) view.findViewById(R.id.media_player_song_image);
 
         Picasso.with(getActivity().getApplicationContext()).load(songImage).into(songImageView);
 
-//        previousButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d(LOG_TAG, "Previous Button Pressed");
-//            }
-//        });
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "Previous Button Pressed");
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "Next Button Pressed");
+            }
+        });
 
         playPauseButton.setImageResource(android.R.drawable.ic_media_play);
 
@@ -59,21 +103,19 @@ public class MediaPlayerFragment extends DialogFragment {
             @Override
             public void onClick(View v) {
 
-
-
                 if (playPause) {
 
                     mediaPlayerIntent.putExtra(SongsActivity.SELECTED_SONG_URL, songUrl);
                     mediaPlayerIntent.putExtra(SongsActivity.SELECTED_SONG_IMAGE, songImage);
-                    mediaPlayerIntent.setAction(ACTION_PLAY);
+                    mediaPlayerIntent.setAction(MediaPlayerService.ACTION_PLAY);
                     getActivity().getApplicationContext().startService(mediaPlayerIntent);
                     playPauseButton.setImageResource(android.R.drawable.ic_media_pause);
                     playPause = false;
 
                 } else if (playPause == false){
                     playPause = true;
-                    mediaPlayerIntent.setAction(ACTION_PAUSE);
-                    getActivity().getApplicationContext().stopService(mediaPlayerIntent);
+                    mediaPlayerIntent.setAction(MediaPlayerService.ACTION_PAUSE);
+                    getActivity().getApplicationContext().startService(mediaPlayerIntent);
                     playPauseButton.setImageResource(android.R.drawable.ic_media_play);
                 }
 
@@ -99,5 +141,15 @@ public class MediaPlayerFragment extends DialogFragment {
         return dialog;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(receiver, new IntentFilter(MediaPlayerService.MEDIA_PLAYER_SEEK));
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(receiver);
+    }
 }
--- app/src/main/java/com/jamescha/spotifystreamer/mediaPlayer/MediaPlayerService.java
+++ app/src/main/java/com/jamescha/spotifystreamer/mediaPlayer/MediaPlayerService.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.jamescha.spotifystreamer.SongsActivity;
@@ -16,14 +17,21 @@ import java.io.IOException;
  */
 public class MediaPlayerService extends Service implements MediaPlayer.OnPreparedListener {
     private final String LOG_TAG = MediaPlayerService.class.getSimpleName();
-    private static final String ACTION_PLAY = "com.jamescha.spotifystreamer.action.PLAY";
-    private static final String ACTION_PAUSE = "com.jamescha.spotifystreamer.action.PAUSE";
+    public static final String ACTION_PLAY = "com.jamescha.spotifystreamer.action.PLAY";
+    public static final String ACTION_PAUSE = "com.jamescha.spotifystreamer.action.PAUSE";
+    public static final String MEDIA_PLAYER_SEEK = "com.jamescha.spottifystream.MediaPlayerService.SEEKBAR";
+    public static final String CURRENT_POSITION = "current_position";
+    public static final String TRACK_LENGTH = "track_length";
     MediaPlayer mMediaPlayer = null;
+    Integer currentPosition;
+
+    LocalBroadcastManager broadcastManager;
 
     public int onStartCommand(Intent intent, int flags, int songId) {
 
+        broadcastManager = LocalBroadcastManager.getInstance(getApplicationContext());
         if (intent != null) {
-            if (intent.getAction().equals(ACTION_PLAY)) {
+            if (intent.getAction().equals(ACTION_PLAY) && mMediaPlayer == null) {
                 String url = intent.getStringExtra(SongsActivity.SELECTED_SONG_URL);
                 mMediaPlayer = new MediaPlayer();
                 mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
@@ -34,7 +42,12 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnPrepare
                 }
                 mMediaPlayer.setOnPreparedListener(this);
                 mMediaPlayer.prepareAsync();
-            } else if (intent.getAction().equals(ACTION_PAUSE)) {
+                updateRunTime.run();
+
+            } else if (intent.getAction().equals(ACTION_PLAY) && mMediaPlayer != null) {
+                mMediaPlayer.start();
+
+            } else if (mMediaPlayer.isPlaying() && intent.getAction().equals(ACTION_PAUSE)) {
                 mMediaPlayer.pause();
             }
         }
@@ -43,7 +56,12 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnPrepare
 
     @Override
     public void onPrepared(MediaPlayer mp) {
+        Intent intent = new Intent(MEDIA_PLAYER_SEEK);
+        intent.putExtra(TRACK_LENGTH, mp.getDuration());
+        broadcastManager.sendBroadcast(intent);
         mp.start();
+
+
     }
 
 
@@ -52,4 +70,18 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnPrepare
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    private Runnable updateRunTime = new Runnable() {
+
+        Intent intent = new Intent(MEDIA_PLAYER_SEEK);
+
+        @Override
+        public void run() {
+            if(mMediaPlayer != null) {
+                currentPosition = mMediaPlayer.getCurrentPosition();
+                intent.putExtra(CURRENT_POSITION, (int) currentPosition);
+                broadcastManager.sendBroadcast(intent);
+            }
+        }
+    };
 }
